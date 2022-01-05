6565a3fa39fc20a2dfd009adecc13db39c638614
Chris Park <cpark2653@gmail.com>
2022-01-04 19:54:34 -0800

    testing

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ let fetch = new Fetch();
 
 document.addEventListener("DOMContentLoaded", async () => {
   await fetch.getIngredients();
-  new Choices('#select-box', {
-    placeholderValue: "search for ingredients",
-    allowHTML: true
-  })
+  // new Choices('#select-box', {
+  //   placeholderValue: "search for ingredients",
+  //   allowHTML: true
+  // })
   modalToggle();
   bindEvents();  
 });
